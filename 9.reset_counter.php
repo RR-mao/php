c5356bd9b3d788d4ce2f8404b2e408d14f44ed37
RR-mao <co0958523240@gmail.com>
2025-04-08 11:43:29 +0800

    9.reset_counter.php

--- 9.reset_counter.php
+++ 9.reset_counter.php
@@ -1,7 +1,9 @@
 <?php
     session_start();
     unset($_SESSION["counter"]);
+    // 顯示重置成功訊息
     echo "counter重置成功....";
+    // 設定2秒後跳轉到"8.counter.php"
     echo "<meta http-equiv=REFRESH content='2; url=8.counter.php'>";
 
 ?>
