28c015d68f72496484d7011e15c0045c6f4c634b
RR-mao <co0958523240@gmail.com>
2025-04-08 11:35:02 +0800

    8.counter.php

--- 8.counter.php
+++ 8.counter.php
@@ -4,7 +4,8 @@
         $_SESSION["counter"]=1;
     else
         $_SESSION["counter"]++;
-
+    
     echo "counter=".$_SESSION["counter"];
+    //會轉到"9.reset_counter.php"來重製counter
     echo "<br><a href=9.reset_counter.php>重置counter</a>";
 ?>
